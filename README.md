f865f1254ecb7968bed7d460c6e4601f558ec92f
I <icaroferre+bitbucket@gmail.com>
2017-08-22 18:10:00 -0300

    Initial Release

---
+++ README.md
@@ -0,0 +1,187 @@
+# About PatchBook:
+
+PatchBook is a markup language and parser for writing and distributing patches for modular synthesizers created by [Spektro Audio](http://spektroaudio.com/).
+The markup language is designed to be easily readable and writeable by humans while the parser can process .txt files written in the PatchBook format and output a JSON file that can be used by other applications to display and process the patch's data.
+
+**Patchbook Version:** 1.0  
+**Parser version:** b1
+
+**Table of Content:**
+
+<!-- MarkdownTOC -->
+
+- Markup Description
+	- Voices
+	- Connections
+	- Parameters
+	- Examples
+- Parser
+	- Requirements
+	- How to Use
+
+<!-- /MarkdownTOC -->
+
+
+---
+
+# Markup Description
+
+---
+
+## Voices
+
+**Voices** must be written in all caps, without any spaces before the name and followed by a colon.  
+Examples:
+
+- BASS 1:
+- VOICE 1:
+- LEAD:
+
+Every connection described after a voice anotation will be assigned to that voice.
+
+---
+
+## Connections
+
+Every connection (patch cable) must be annotated using the following format: **- Output Module (Output Label) >> Input Module (Input Label)**.  
+Examples:  
+
+- Maths (Ch. 1 Unity) >> Polaris (CV 2)
+- Tides (Bi) >> Braids (Timbre)
+
+While the >> indicator can be used to indicate a standard connection, it could (and should) also be replaced by more specific indicators according to the kind of signal being sent from the Output Module to the Input Module:  
+
+- \>> for CV
+- -> for Audio
+- p> for Pitch (1v/oct or Hz/V)
+- g> for Gate
+- t> for Trigger
+- c> for Clock
+
+Examples:
+
+- Metropolis (Pitch) p> Braids (1 V/Oct)
+- Pamela's Workout (1) c> Penta (Clk)
+- Braids (Out) -> Polaris (Input)
+
+**Additional info:**
+
+- The manufacture's name should only be included if the module's name is too generic (example: VB Modular ADSR).
+- Non-modular equipment (such as audio interfaces, recorders, and other synths) should be written in all caps: NAME OF GEAR (Input or Output).
+
+---
+
+## Parameters
+
+Parameters can be annotated in 2 different ways: single line or multiline. Every parameter annotation must start with an asterisk character before the module name.
+
+**Single-line**  
+\* Function: Rise = 50% | Fall = 50% | Curve = 30%
+
+**Multi-Line**  
+\* Braids:  
+	| Mode = CSAW  
+	| Color = 50%  
+	| Timbre = 50%  
+	
+**Additional info**
+
+- Parameter values can be written as knob / fader position (percentage), specific value followed by unit (5Hz, 10ms, etc), or as a descriptive value (fast, slow, simple, complex, short, long).
+- Parameters are not assigned to any voice since the same module can be used in multiple voices. 
+ 
+
+---
+
+## Examples
+
+---
+
+### Example 1
+
+```
+VOICE 1:
+	- Metropolis (Pitch) p> Braids (1v/oct)
+	- Metropolis (Gate) g> Function (Trigger)
+	- Braids (Out) -> Optomix (Ch1 Signal)
+	- Function (+ Out) >> Optomix (Ch1 CV)
+	- Function (- Out) >> Braids (Timbre CV)
+	- Optomix (Out 1) -> AUDIO INTERFACE (input)
+	
+	* Metropolis:
+	| BPM = 124
+	| Swing = 0
+	| Root = F
+	| Scale = Minor
+	| Mode = F. Forward
+	| Stages = 16
+	
+	* Braids:
+	| Mode = Fold
+	| Timbre = 30%
+	| Timbre CV = -20%
+	| Color = 0%
+
+	* Function: Rise = 50% | Fall = 50% | Curve = 30%
+	* Optomix: Damp = 0% | Control = 100%
+```
+
+### Example 2
+
+```
+VOICE 1:
+
+	- Metropolis (Pitch) p> Aether VCO (CV)
+	- Metropolis (Gate) g> Maths (Ch 1 Trigger)
+	- Metropolis (Gate) g> Maths (Ch 4 Trigger)
+	
+	* Aether VCO: LFO Freq = 5 | LFO PWM = 7
+	- Aether VCO (Pulse) -> Mixer (Ch1)
+	- Aether VCO (Sub 1) -> Tides (Clk)
+	- Tides (Bi) -> Mixer (Ch2)
+	- Aether VCO (Sub 2) -> Z3000 (HSync)
+	- Z3000 (Saw) -> Mixer (Ch3)
+	
+	- MultiLFO (LFO 1) >> Tides (Smoothness)
+	- MultiLFO (LFO 2 Triangle) >> Tides (Shape)
+	- MultiLFO (LFO 3 Triangle) >> Z3000 (PWM)
+	* MultiLFO:
+	| LFO 1 Freq = 3.8
+	| LFO 1 Shape = Sine
+	| LFO 1 S&H = 0
+	| LFO 2 Freq = 1
+	| LFO 3 Freq = 1
+	* Tides: PLL Mode = True | Freq = 60% | Smoothness = 70%
+	* Z3000: Freq = 1pm
+	
+	- Maths (Ch 1) >> Multifilter (CV)
+	- Maths (Ch 4) >> uVCA (Ch1 CV)
+	
+	- Mixer (Output) -> Multifilter (Input)
+	- Multifilter (LPF) -> uVCA (Ch1 Input)
+	- uVCA (Ch1 Output) -> AUDIO INTERFACE (In 3)
+
+```
+
+----
+
+# Parser
+
+The PatchBook parser is a Python program that can read text files written in the PatchBook format and generate a JSON file.
+
+---
+## Requirements
+
+-  Python 3
+
+---
+## How to Use
+
+To use the parser, download the python script, open the terminal and use the command:
+
+```python3 path/to/script/patchbook.py -file /path/to/textfile.txt```
+
+After loading the text file into the parser, you can use the following commands to process it:
+
+-	**module**: Outputs a list of connections and parameters for a specific module.
+- **connections**: Prints a list of all connections organized by type (pitch, gate, clock, etc).
+- **export**: Generates a JSON file based on the input text file.
---
+++ patchbook.py
@@ -0,0 +1,303 @@
+"""
+PATCHBOOK MARKUP LANGUAGE & PARSER
+CREATED BY SPEKTRO AUDIO
+http://spektroaudio.com/
+"""
+
+import sys
+import re
+import os
+import argparse
+import json
+
+# Parser INFO
+parserVersion = "b1"
+
+# Reset main dictionary
+mainDict = {}
+
+# Available connection types
+connectionTypes = {
+    "->" : "audio",
+    ">>": "cv",
+    "p>": "pitch",
+    "g>": "gate",
+    "t>": "trigger",
+    "c>" : "clock"
+}
+
+
+# Reset global variables
+lastModuleProcessed = ""
+lastVoiceProcessed = ""
+
+# Parse script arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-file", type=str, default="",
+                        help="Name of the text file that will be parsed (including extension)")
+parser.add_argument("-debug", type=int, default=0,
+                    help="Enable Debugging Mode")
+args = parser.parse_args()
+filename = args.file
+debugMode = args.debug
+
+# Set up debugMode
+if args.debug == 1:
+    debugMode = True
+else:
+    debugMode = False
+
+def initial_print():
+    print()
+    print("██████████████████████████████")  
+    print("       PATCHBOOK PARSER       ")
+    print("   Created by Spektro Audio   ")
+    print("██████████████████████████████")
+    print()
+    print("Version " + parserVersion)
+    print()
+
+
+def get_script_path():
+    # Get path to python script
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+
+
+def getFilePath(filename):
+    try:
+        # Append script path to the filename
+        base_dir = get_script_path()
+        filepath = os.path.join(base_dir, filename)
+        if debugMode: print("File path: " + filepath)
+        return filepath
+    except IndexError:
+        pass
+
+def parseFile(filename):
+    # This function reads the txt file and process each line.
+    lines = []
+    try:
+        print("Loading file: " + filename)
+        with open(filename, "r") as file:
+            for l in file:
+                lines.append(l)
+                regexLine(l)
+    except TypeError:
+        print("ERROR. Please add text file path after the script.")
+    except FileNotFoundError:
+        print("ERROR. File not found.")
+    print("File successfully processed.")
+    print()
+
+
+def regexLine(line):
+    global lastModuleProcessed
+    global lastVoiceProcessed
+
+    if debugMode: print()
+    if debugMode: print("Processing: " + line)
+
+    # CHECK FOR VOICES
+    if debugMode: print("Cheking input for voices...")
+    re_filter = re.compile(r"^([^\*]).+")  # Regex for "VOICE 1:"
+    re_results = re_filter.search(line)
+    try:
+        # For some reason the Regex filter was still detecting parameter declarations as voices, 
+        # so I'm also running the results through an if statement.
+        results = re_results.group().replace(":", "") 
+        if "*" not in results and "-" not in results and "|" not in results:
+            if debugMode: print("New voice found: " + results.upper())
+            lastVoiceProcessed = results.upper()
+    except AttributeError:
+        pass
+
+    # CHECK FOR CONNECTIONS
+    if debugMode: print("Cheking input for connections...")
+    re_filter = re.compile(r"\-\s(.+)[(](.+)[)]\s(\>\>|\-\>|[a-z]\>)\s(.+)[(](.+)[)]")
+    re_results = re_filter.search(line)
+    try:
+        results = re_results.groups()
+        voice = lastVoiceProcessed
+        if len(results) == 5:
+            if debugMode: print("New connection found, parsing info...")
+            addConnection(results, voice)
+    except AttributeError:
+        pass
+
+    # CHECK PARAMETERS
+    if debugMode: print("Checking for parameters...")
+    # If single-line parameter declaration:
+    if ":" in line and "*" in line:
+        # Get module name
+        module = line.split(": ")[0].replace("*", "").strip().lower()
+        if debugMode: print("New module found: " + module)
+        try:
+            # If parameters are also declared
+            parameters = line.split(": ")[1].split(" | ")
+            for p in parameters:
+                p = p.split(" = ")
+                addParameter(module, p[0].strip().lower(), p[1].strip()) 
+        except IndexError:
+            if debugMode: print("No parameters found. Storing module as global variable...")
+            lastModuleProcessed = module.replace(":", "").strip()
+
+    # If multi-line parameter declaration:
+    if "|" in line and "=" in line and "*" not in line:
+        module = lastModuleProcessed.lower()
+        if debugMode: print("Using global variable: " + module)
+        parameter = line.split(" = ")[0].replace("|", "").strip().lower()
+        value = line.split(" = ")[1].strip()
+        addParameter(module, parameter, value)
+
+
+def addConnection(list, voice="none"):
+    global mainDict
+    global connectionTypes
+
+    if debugMode: print("Adding new connection...")
+    if debugMode: print("-----")
+
+    output_module = list[0].lower().strip()
+    output_port = list[1].lower().strip()
+
+    if debugMode: print("Output module: " + output_module)
+    if debugMode: print("Output port: " + output_port)
+
+    try:
+        connection_type = connectionTypes[list[2].lower()]
+        if debugMode: print("Matched connection type: " + connection_type)
+    except KeyError:
+        print("Invalid connection: " + list[2])
+        connection_type = "cv"
+
+    input_module = list[3].lower().strip()
+    input_port = list[4].lower().strip()
+
+    if debugMode: print("Input module: " + input_module)
+    if debugMode: print("Input port: " + output_port)
+
+    checkModuleExistance(output_module, output_port, "out")
+    checkModuleExistance(input_module, input_port, "in")
+
+    if debugMode: print("Appending output and input connections to mainDict...")
+    mainDict[output_module]["connections"]["out"][output_port].append((input_module, input_port, connection_type, voice))
+    mainDict[input_module]["connections"]["in"][input_port] = [output_module, output_port, connection_type, voice]
+    if debugMode: print("-----")
+
+
+def checkModuleExistance(module, port="port", direction=""):
+    global mainDict
+
+    if debugMode: print("Checking if module already existing in main dictionary: " + module)
+
+    # Check if module exists in main dictionary
+    if module not in mainDict:
+        mainDict[module] = {}
+        mainDict[module]["parameters"] = {}
+        mainDict[module]["connections"] = {}
+        mainDict[module]["connections"]["out"] = {}
+        mainDict[module]["connections"]["in"] = {}
+
+    # If it exists, check if the port exists
+    if direction == "in":
+        if port not in mainDict[module]["connections"]["in"]:
+            mainDict[module]["connections"]["in"][port] = []
+
+    if direction == "out":
+        if port not in mainDict[module]["connections"]["out"]:
+            mainDict[module]["connections"]["out"][port] = []
+
+
+def addParameter(module, name, value):
+    checkModuleExistance(module)
+    # Add parameter to mainDict
+    if debugMode: print("Adding parameter: " + module + " - " + name + " - " + value)
+    mainDict[module]["parameters"][name] = value
+
+
+def askCommand():
+    command = input("> ").lower().strip()
+    if command == "module":
+        detailModule()
+    elif command == "print":
+        printDict()
+    elif command == "export":
+        exportJSON()
+    elif command == "connections":
+        printConnetions()
+    else:
+        print("Invalid command, please try again.")
+    askCommand()
+
+
+def detailModule():
+    global mainDict
+    module = input("Enter module name: ").lower()
+    if module in mainDict:
+        print("-------")
+        print("Showing information for module: " + module.upper())
+        print()
+        print("Inputs:")
+        for c in mainDict[module]["connections"]["in"]:
+            keyvalue = mainDict[module]["connections"]["in"][c]
+            print(keyvalue[0].title() + " (" + keyvalue[1].title() + ") > " + c.title() + " - " + keyvalue[2].title())
+        print()
+
+        print("Outputs:")
+        for x in mainDict[module]["connections"]["out"]:
+            port = mainDict[module]["connections"]["out"][x]
+            for c in port:
+                # print(str(c))
+                keyvalue = c
+                print(x.title() + " > " + keyvalue[0].title() + " (" + keyvalue[1].title() + ") " + " - " + keyvalue[2].title() + " - " + keyvalue[3])
+        print()
+
+        print("Parameters:")
+        for p in mainDict[module]["parameters"]:
+            value = mainDict[module]["parameters"][p]
+            print(p.title() + " = " + value)
+        print()
+
+        print("-------")
+
+
+def printConnetions():
+    print()
+    print("Printing all connections by type...")
+    print()
+
+    for ctype in connectionTypes:
+        ctype_name = connectionTypes[ctype]
+        print("Connection type: " + ctype_name)
+        # For each module
+        for module in mainDict:
+            # Get all outgoing connections:
+            connections = mainDict[module]["connections"]["out"]
+            for c in connections:
+                connection = connections[c]
+                for subc in connection:
+                    # print(connection)
+                    if subc[2] == ctype_name:
+                        print(module.title() + " > " + subc[0].title() + " (" + subc[1].title() + ") ")
+        print()
+
+
+def exportJSON():
+    # Exports mainDict as json file
+    name = filename.split(".")[0]
+    filepath = getFilePath(name + '.json')
+    print("Exporting dictionary as file: " + filepath)
+    with open(filepath, 'w') as fp:
+        json.dump(mainDict, fp)
+
+
+def printDict():
+    global mainDict
+    for key in mainDict:
+        print(key.title() + ": " + str(mainDict[key]))
+
+
+if __name__ == "__main__":
+    initial_print()
+    parseFile(getFilePath(filename))
+    askCommand()
